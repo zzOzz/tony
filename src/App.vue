1f2aaf650ff93f7980ca85a632bb59ae02b238cb
Vincent Lombard <vincent.lombard@universite-lyon.fr>
2018-05-31 13:47:13 +0200

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tony msg="Click"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tony from './components/Tony.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Tony
   }
 }
 </script>
---
+++ src/components/Tony.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <img class="tony" src="../assets/tony.svg"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tony',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.tony {
+    width: 50vw;
+}
+</style>
